4c125329a217ecd5a9f83be9079a3fd90a9d7d21
powion <phensman@kth.se>
2016-05-05 12:31:29 +0200

    Add storybot files for new subreddits, refactor process_files.py

--- src/crawler/process_files.py
+++ src/crawler/process_files.py
@@ -1,38 +1,40 @@
 import sys
 import re, string
 
-def cleanfile_topics(filename):
-    pattern = re.compile('[^a-z ]', re.UNICODE)
+all_patterns = [
+              re.compile('[^a-z ]', re.UNICODE),
+              re.compile('(\w*http\w+\s?|\w+http\w*\s?)', re.UNICODE),
+              re.compile('http[^\s]+', re.UNICODE)
+        ]
+
+def cleanfile(filename, patterns):
     with open(filename, 'r') as infile:
         text = infile.read().lower()
         text = re.sub('\n', ' ', text)
-        text = pattern.sub('', text)
+        for pattern in patterns:
+            text = pattern.sub('', text)
+        text = re.sub('\s+', ' ', text)
         return text
 
-def cleanfile(filename):
-    with open(filename, 'r') as infile:
-        text = infile.read().lower()
-        text = re.sub('\n', ' ', text)
-        return text
-
-def storybot_file(path, N):
-    with open(path + 'storybot.txt', 'w') as file:
-        for i in range (0, N):
-            file.write(cleanfile(path + str(i) + '.txt'))
-
-def topics_file(path, N):
-    with open(path + 'topics.txt', 'w') as file:
-        for i in range (0, N):
-            file.write(cleanfile_topics(path + str(i) + '.txt') + '\n')
+def process_file(path, outname, patterns, lineend):
+    with open(path + outname + '.txt', 'w') as file:
+        i = 0
+        try:
+            while True:
+                file.write(cleanfile(path + str(i) + '.txt', patterns) + lineend)
+                i += 1
+        except IOError:
+            print(i, 'files processed.')
+            return
 
 def main(argv):
-    if len(argv) == 2:
-        storybot_file(argv[0], int(argv[1]))
-    elif len(argv) == 3:
-        topics_file(argv[0], int(argv[1]))
+    if len(argv) == 1:
+        process_file(argv[0], 'storybot', [all_patterns[2]], '')
+    elif len(argv) == 2:
+        process_file(argv[0], 'topics', all_patterns[:2], '\n')
     else:
         print("Insufficient arguments:")
-        print("process_files.py [path] [N] [-t]")
+        print("process_files.py path [-t]")
         return
 
 if __name__ == "__main__":
